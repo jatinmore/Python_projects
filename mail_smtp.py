f4798e47f13d9064f1c354ca846cb684c2a5b7ef
jatinmore <54055686+jatinmore@users.noreply.github.com>
2019-12-15 11:21:51 +0530

    Delete mail_smtp.py

--- mail_smtp.py
+++
@@ -1,7 +0,0 @@
-import smtplib
-server=smtplib.SMTP('smtp.gmail.com',587)
-server.ehlo()
-server.starttls()
-server.login('SENDER ID','PASSWORD')
-server.sendmail('SENDER ID','RECIEVER ID','Subject: \n\n MSG')
-server.quit()
